b0f4e5fb374ed6f36d156bc802d63605660b3cf1
Touraj Khatibi <2rajpx@gmail.com>
2023-03-09 19:44:25 +0800

    refactor: change jest.config

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+const { getJestProjects } = require("@nrwl/jest");
+
+/** @type {import('jest').Config} */
+const config = {
+  // projects: getJestProjects(),
+  preset: "ts-jest",
+  testEnvironment: "node",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
+  transform: {
+    "^(?!.*\\.(js|jsx|ts|tsx|css|json)$)": "@nrwl/react/plugins/jest",
+    "^.+\\.[tj]sx?$": "babel-jest",
+  },
+};
+
+module.exports = config;
